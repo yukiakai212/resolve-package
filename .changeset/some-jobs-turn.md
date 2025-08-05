c75a5effc2bf78e9a2e8bf01244c6767b44b1eaa
Yuki <yuki.akai.rz.212@gmail.com>
2025-08-05 17:41:20 +0700

    pre-release: TS support

---
+++ .changeset/some-jobs-turn.md
@@ -0,0 +1,5 @@
+---
+'@yukiakai/resolve-package': minor
+---
+
+TS support
--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,32 @@
 import path from 'node:path';
 import fs from 'node:fs';
-import resolveSync from 'resolve/sync.js';
+import { sync as resolveSync } from 'resolve';
 import { findUp } from '@yukiakai/find-up';
 
-export const resolvePath = (pkgName, basedir) => {
+export const resolvePath = (pkgName: string, basedir: string = process.cwd()): string | null => {
   try {
     const pkgFilePath = resolveSync(pkgName, { basedir });
     return pkgFilePath
       ? findUp('package.json', {
           basedir: path.dirname(pkgFilePath),
           matcher: (file) => {
-            const json = JSON.parse(fs.readFileSync(file));
+            const json = JSON.parse(fs.readFileSync(file).toString());
             if (json.name) return true;
           },
-        })
+        }) || null
       : null;
   } catch {
     return null;
   }
 };
-export const resolveMetadata = (pkgName, basedir) => {
+export const resolveMetadata = (
+  pkgName: string,
+  basedir?: string,
+): Record<string, string | null | number | object | object[]> | null => {
   const pkgPath = resolvePath(pkgName, basedir);
   if (!pkgPath) return null;
   try {
-    return JSON.parse(fs.readFileSync(path.join(pkgPath, 'package.json')));
+    return JSON.parse(fs.readFileSync(path.join(pkgPath, 'package.json')).toString());
   } catch {
     return null;
   }
